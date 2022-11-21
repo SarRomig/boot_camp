4b4abf56d30b595a7cffaaca1c2813ad36b90fe4
SARAH ROMIG <sarromig@gmail.com>
2022-11-21 13:26:46 -0500

    connect4 js pseudo and handleClick adds

--- BootCamp22-23/Bootcamp22/connect_4/connect4.js
+++ BootCamp22-23/Bootcamp22/connect_4/connect4.js
@@ -19,7 +19,6 @@
    // TODO: set "board" to empty HEIGHT x WIDTH matrix array
    //should set the global board variable to be an array of 6 arrays (height), each containing 7 items (width) - board[y][x]
    //for loop to populate subarrays  -- array of arrays
-  
   let arr = [];
   for (let i=0;i<rows;i++) { //rows will reach 6 and stop, so it will end up with 6 rows of empty arrays - can pass in WIDTH constant as rows?
      arr[i] = []; //how to designate 7 empty spaces within this array? or does that not matter until it is in action - seems to be... working?
@@ -28,50 +27,52 @@
   return arr;
 }
  
-
  /** makeHtmlBoard: make HTML table and row of column tops. */
  
  function makeHtmlBoard() {
    // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
     const htmlBoard = document.querySelector("#board");
    // TODO: add comment for this code
-   const top = document.createElement("tr");
+   const top = document.createElement("tr"); //add new table row with column top id to have a dashed border and hover feature with gold background then adding a click listener to the top row of the table. handleClick will populate the player's piece within the given "x" column
    top.setAttribute("id", "column-top");
    top.addEventListener("click", handleClick);
- 
+
    for (let x = 0; x < WIDTH; x++) { 
      const headCell = document.createElement("td");
      headCell.setAttribute("id", x);
      top.append(headCell);
    }
-   htmlBoard.append(top);
+   htmlBoard.append(top); //physically adding top row to htmlBoard element
  
    // TODO: add comment for this code
-   for (let y = 0; y < HEIGHT; y++) {
+   for (let y = 0; y < HEIGHT; y++) { //If height = 6, this first for loop adds 6 new table rows
      const row = document.createElement("tr");
-     for (let x = 0; x < WIDTH; x++) {
+     for (let x = 0; x < WIDTH; x++) { // this second for loop sets the cells into the table row as td's with the y,x attribute which will populate into findSpotForCol
        const cell = document.createElement("td");
        cell.setAttribute("id", `${y}-${x}`);
-       row.append(cell);
+       row.append(cell); //physically adds the new cells to each table row, 7 across because width = 7
      }
-     htmlBoard.append(row);
+     htmlBoard.append(row); //physically adds the new rows, 7 across, to the htmlBoard element
    }
  }
  
  /** findSpotForCol: given column x, return top empty y (null if filled) */
- 
+ //somewhere, need to update board variable to reflect htmlBoard -- I think?
  function findSpotForCol(x) {
    // TODO: write the real version of this, rather than always returning 0
-
-   return 0;
+   //this will run again for the next x it's given so return null if there are no empty y's
+    //use board variable and get id with y,x array information, then add 1 to y to get the top empty y position
+  
+   
+   return 0; //return top empty y position (tr - row) with id of y,x
  }
  
  /** placeInTable: update DOM to place piece into HTML table of board */
  
  function placeInTable(y, x) {
-   // TODO: make a div and insert into correct table cell
-   //identify correct table cell from findSpot ForCol? then add class based on player1 or player2 to determine color. setAtritribute for .piece and .player1 - to start
-   //add div to correct "td" cell in the board. headCell for x and cell for y
+   // TODO: make a div and insert into correct table cell 
+   //create new div and add (toggle?) class based on player1 or player2 to determine color. setAtritribute for .piece and .player1 - to start
+   //add div to correct "td" cell in the board. identify correct table cell from findSpotForCol based on id of board then append the new div and 
 
 
  }
@@ -110,7 +111,15 @@
  
    // switch players
    // TODO: switch currPlayer 1 <-> 2
-   //currPlayer = 2, set id to .player2
+   //set id to .player2 (toggle id?) 
+   if (currPlayer == 1) { 
+    currPlayer = 2;
+      currPlayer.setAttribute(id, "player2");
+    }; 
+   else if (currPlayer == 2) {
+     currPlayer = 1;
+     currPlayer.setAttribute(id, "player1");
+    };
  }
  
  /** checkForWin: check board cell-by-cell for "does a win start here?" */

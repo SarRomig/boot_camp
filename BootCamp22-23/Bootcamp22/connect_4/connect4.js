4b37efc2793e93fc3379d9c1c550072cb82bfaf1
SARAH ROMIG <sarromig@gmail.com>
2022-11-21 12:15:10 -0500

    connect4 updates

--- BootCamp22-23/Bootcamp22/connect_4/connect4.js
+++ BootCamp22-23/Bootcamp22/connect_4/connect4.js
@@ -5,41 +5,51 @@
  * board fills (tie)
  */
 
- var WIDTH = 7;
- var HEIGHT = 6;
+ const WIDTH = 7;
+ const HEIGHT = 6;
  
- var currPlayer = 1; // active player: 1 or 2
- var board = []; // array of rows, each row is array of cells  (board[y][x])
+ let currPlayer = 1; // active player: 1 or 2
+ const board = []; // array of rows, each row is array of cells  (board[y][x])
  
  /** makeBoard: create in-JS board structure:
   *    board = array of rows, each row is array of cells  (board[y][x])
   */
- 
- function makeBoard() {
+ //
+ function makeBoard(rows) {
    // TODO: set "board" to empty HEIGHT x WIDTH matrix array
- }
+   //should set the global board variable to be an array of 6 arrays (height), each containing 7 items (width) - board[y][x]
+   //for loop to populate subarrays  -- array of arrays
+  
+  let arr = [];
+  for (let i=0;i<rows;i++) { //rows will reach 6 and stop, so it will end up with 6 rows of empty arrays - can pass in WIDTH constant as rows?
+     arr[i] = []; //how to designate 7 empty spaces within this array? or does that not matter until it is in action
+      
+  }
+  return arr;
+}
  
+
  /** makeHtmlBoard: make HTML table and row of column tops. */
  
  function makeHtmlBoard() {
    // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
- 
+    const htmlBoard = document.querySelector("#board");
    // TODO: add comment for this code
-   var top = document.createElement("tr");
+   const top = document.createElement("tr");
    top.setAttribute("id", "column-top");
    top.addEventListener("click", handleClick);
  
-   for (var x = 0; x < WIDTH; x++) {
-     var headCell = document.createElement("td");
+   for (let x = 0; x < WIDTH; x++) {
+     const headCell = document.createElement("td");
      headCell.setAttribute("id", x);
      top.append(headCell);
    }
    htmlBoard.append(top);
  
    // TODO: add comment for this code
-   for (var y = 0; y < HEIGHT; y++) {
+   for (let y = 0; y < HEIGHT; y++) {
      const row = document.createElement("tr");
-     for (var x = 0; x < WIDTH; x++) {
+     for (let x = 0; x < WIDTH; x++) {
        const cell = document.createElement("td");
        cell.setAttribute("id", `${y}-${x}`);
        row.append(cell);
@@ -59,6 +69,7 @@
  
  function placeInTable(y, x) {
    // TODO: make a div and insert into correct table cell
+   //identify correct table cell from findSpot ForCol?
  }
  
  /** endGame: announce game end */
@@ -71,10 +82,10 @@
  
  function handleClick(evt) {
    // get x from ID of clicked cell
-   var x = +evt.target.id;
+   let x = +evt.target.id;
  
    // get next spot in column (if none, ignore click)
-   var y = findSpotForCol(x);
+   const y = findSpotForCol(x);
    if (y === null) {
      return;
    }
@@ -115,12 +126,12 @@
  
    // TODO: read and understand this code. Add comments to help you.
  
-   for (var y = 0; y < HEIGHT; y++) {
-     for (var x = 0; x < WIDTH; x++) {
-       var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-       var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-       var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-       var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+   for (let y = 0; y < HEIGHT; y++) {
+     for (let x = 0; x < WIDTH; x++) {
+       const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+       const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+       const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+       const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
  
        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
          return true;
